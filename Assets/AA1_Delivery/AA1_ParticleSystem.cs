40dd8d1ede018d38c4bb5d9346043821ebfef611
Judit Espigol <judith.espigol@enti.cat>
2024-03-07 13:22:37 +0100

    clean poolParticles code

--- Assets/AA1_Delivery/AA1_ParticleSystem.cs
+++ Assets/AA1_Delivery/AA1_ParticleSystem.cs
@@ -1,8 +1,11 @@
 using System;
+using static UnityEditor.Rendering.ShadowCascadeGUI;
 
 [System.Serializable]
 public class AA1_ParticleSystem
 {
+    Random rnd = new Random();
+
     [System.Serializable]
     public struct Settings
     {
@@ -89,61 +92,143 @@ public class AA1_ParticleSystem
         public Vector3C position;
         public Vector3C velocity;
         public Vector3C aceleration;
-    }
-    Random rnd = new Random();
 
-    float timer = 0.0f;
-    float spawnTime = 0.0f; 
+        public void InitParticle(SettingsParticle settings)
+        {
+            this.active = true;
+
+            this.size = settings.size;
+            this.mass = settings.mass;
+
+            this.aceleration = Vector3C.zero;
+            this.velocity = Vector3C.zero;
+        }
+
+        public void InitParticleInCascade(SettingsCascade settingsCascade)
+        {
+            Random rnd = new Random();
+
+            this.lifeTime = rnd.Next((int)settingsCascade.minParticlesLifeTime, (int)settingsCascade.maxParticlesLifeTime);
+
+            LineC lineBetweenCascades = LineC.CreateLineFromTwoPoints(settingsCascade.PointA, settingsCascade.PointB);
+            // EQ. PARAMETRICA: r(x) = B + x * direction x = 0..1
+            this.position = lineBetweenCascades.origin + (lineBetweenCascades.direction * (float)rnd.NextDouble());
+
+            this.force = new Vector3C
+                (rnd.Next((int)(settingsCascade.Direction.x * settingsCascade.minImpulse), (int)(settingsCascade.Direction.x * settingsCascade.maxImpulse)),
+                rnd.Next((int)(settingsCascade.Direction.y * settingsCascade.minImpulse), (int)(settingsCascade.Direction.y * settingsCascade.maxImpulse)),
+                rnd.Next((int)(settingsCascade.Direction.z * settingsCascade.minImpulse), (int)(settingsCascade.Direction.z * settingsCascade.maxImpulse)));
+        }
+
+        public void InitParticleInCannon(SettingsCannon settingsCannon)
+        {
+            Random rnd = new Random();
+
+            this.lifeTime = rnd.Next((int)settingsCannon.minParticlesLifeTime, (int)settingsCannon.maxParticlesLifeTime);
+
+            this.position = settingsCannon.Start;
+
+            this.force = Vector3C.zero;
+        }
+
+        public bool CheckLifeTime()
+        {
+            if(this.lifeTime < 0.0f)
+            {
+                this.active = false;
+                return true;
+            }
+            return false;
+
+        }
+
+        public void Euler(Settings settings, float dt)
+        {
+            // Apply forces
+            this.force += settings.gravity;
+
+            // Calculate acceleration, velocity and position
+            this.aceleration = this.force / this.mass;
+            this.velocity = this.velocity + this.aceleration * dt;
+            this.position = this.position + this.velocity * dt;
+            this.force = Vector3C.zero;
+
+            // Clean forces
+            this.force = Vector3C.zero;
+        }
+    }
+
+    bool start = true;
+
+    float timerOneSecond; 
+    float spawnTime, lastTimeSpawned;
+
     Particle[] particles = null;
 
     public Particle[] Update(float dt)  
     {
+        if(start)
+            Start();
 
-        if(timer == 0.0f)
-        {
-            InitPoolingParticles();
-        }
-        timer += dt;
+        if (lastTimeSpawned > spawnTime)
+            SpawnParticle(dt);
 
-        CreateParticle(dt); 
 
         for (int i = 0; i < particles.Length; ++i)
         {
             if (particles[i].active)
             {
-                // Apply forces
-                particles[i].force += settings.gravity;
+                // 1. Comprobar el tiempo de vida
+                if (particles[i].CheckLifeTime()) { continue; }
+                particles[i].lifeTime -= dt;
 
-                // Calculate acceleration, velocity and position
-                particles[i].aceleration = particles[i].force / particles[i].mass;
-                particles[i].velocity = particles[i].velocity + particles[i].aceleration * dt;
-                particles[i].position = particles[i].position + particles[i].velocity * dt;
-                particles[i].force = Vector3C.zero;
+                // 2. Calcular euler
+                particles[i].Euler(settings, dt);
 
-                // Clean forces
-                particles[i].force = Vector3C.zero;
             }
             else
             {
-                // Calculate acceleration, velocity and position
-                particles[i].aceleration = Vector3C.zero;
-                particles[i].velocity = Vector3C.zero;
+                // 1. Spawnear fuera de la pantalla
                 particles[i].position = Vector3C.one * 100;
-                particles[i].force = Vector3C.zero;
             }
         }
+
+        // Si ha pasado 1 segundo canvia el numero de particulas a spawner por segundo
+        if (timerOneSecond > 1.0f)
+        {
+            timerOneSecond -= 1.0f;
+            spawnTime = NewSpawnTime(); 
+        }
+
+        // Sumamos los timers
+        timerOneSecond += dt;
+        lastTimeSpawned += dt;
+
         return particles;
     }
 
-    public void InitPoolingParticles()
+    public void Start()
     {
+        start = false;
+
         settings.poolCount = 0;
         particles = new Particle[settings.objectPoolingParticles];
+
+       spawnTime = NewSpawnTime();
     }
 
-    public int NumParticlesToSpawn()
+    public float NewSpawnTime()
     {
+        int particlesPorSecond = RandomParticlesToSpawn(); 
 
+        if(particlesPorSecond != 0)
+            return 1.0f / particlesPorSecond;
+
+        return 1.0f;
+    }
+
+    public int RandomParticlesToSpawn()
+    {
         switch (emissionMode.mode)
         {
             case EmissionMode.Emission.CASCADE:
@@ -155,51 +240,27 @@ public class AA1_ParticleSystem
         }
     }
 
-    public void CreateParticle(float dt)
+    public void SpawnParticle(float dt)
     {
-        spawnTime += NumParticlesToSpawn() * dt;
-        if (spawnTime < 1.0f)
-        {
-            return;
-        }
-        spawnTime -= 1.0f;
-
+        // 1. Tiene que spawner alguna particula
+        lastTimeSpawned -= spawnTime;
         settings.poolCount++;
-        if(settings.poolCount >= settings.objectPoolingParticles) 
-        {
-            settings.poolCount = 0;
-        }
 
+        // 2. Comprobar que el pool count no sobrepase la array
+        if (settings.poolCount >= settings.objectPoolingParticles) { settings.poolCount = 0; }
+
+        // 3. Comprobar que no esta activa
         if (particles[settings.poolCount].active) { return; }
 
-        particles[settings.poolCount].active = true;
-
-        particles[settings.poolCount].size = settingsParticle.size;
-        particles[settings.poolCount].mass = settingsParticle.mass;
-
+        // 4. Si no esta activa la seteamos
+        particles[settings.poolCount].InitParticle(settingsParticle); 
         switch (emissionMode.mode)
         {
             case EmissionMode.Emission.CASCADE:
-
-                particles[settings.poolCount].lifeTime = rnd.Next((int)settingsCascade.minParticlesLifeTime, (int)settingsCascade.maxParticlesLifeTime);
-
-                LineC lineBetweenCascades = LineC.CreateLineFromTwoPoints(settingsCascade.PointA, settingsCascade.PointB);
-                // EQ. PARAMETRICA: r(x) = B + x * direction x = 0..1
-                particles[settings.poolCount].position = lineBetweenCascades.origin + (lineBetweenCascades.direction * (float)rnd.NextDouble());
-
-                particles[settings.poolCount].force = new Vector3C
-                    (rnd.Next((int)(settingsCascade.Direction.x * settingsCascade.minImpulse), (int)(settingsCascade.Direction.x * settingsCascade.maxImpulse)),
-                    rnd.Next((int)(settingsCascade.Direction.y * settingsCascade.minImpulse), (int)(settingsCascade.Direction.y * settingsCascade.maxImpulse)),
-                    rnd.Next((int)(settingsCascade.Direction.z * settingsCascade.minImpulse), (int)(settingsCascade.Direction.z * settingsCascade.maxImpulse)));
+                particles[settings.poolCount].InitParticleInCascade(settingsCascade); 
                 break;
             case EmissionMode.Emission.CANNON:
-
-                particles[settings.poolCount].lifeTime = rnd.Next((int)settingsCannon.minParticlesLifeTime, (int)settingsCannon.maxParticlesLifeTime);
-
-                particles[settings.poolCount].position = settingsCannon.Start;
-
-                particles[settings.poolCount].force = Vector3C.zero;
-
+                particles[settings.poolCount].InitParticleInCannon(settingsCannon);
                 break;
             default:
                 break;
